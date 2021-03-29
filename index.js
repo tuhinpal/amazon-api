a72113a288e9eb2c60da7fdff5893237c5ac8ed4
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2021-03-29 12:37:03 +0300

    index.js Edited

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ import search from './search'
 import header from './header'
 import product from './product'
 
-async function handleRequest(request) { /* Handle the incoming request */
+async function handleRequest(request) { // Handle the incoming request 
     const headers = header(request.headers)
-    const path = new URL(request.url).pathname; /* Get the pathname */
+    const path = new URL(request.url).pathname; // Get the pathname 
 
-    if (request.method === 'GET') { /* Respond for GET request method */
-        if (path.startsWith('/search/')) { /* Search */
+    if (request.method === 'GET') { // Respond for GET request method 
+        if (path.startsWith('/search/')) { // Search
             return new Response(await search(path.replace('/search/', ''), request.headers.get("host")), {
                 status: 200,
                 headers
             })
-        } else if (path.startsWith('/product/')) { /* Product Page */
+        } 
+        else if (path.startsWith('/product/')) { // Product Page 
             return new Response(await product(path.replace('/product/', '')), {
                 status: 200,
                 headers
             })
-        } else {
+        } 
+        else {
             return Response.redirect("https://github.com/cachecleanerjeet/amazon-scraper", 301)
         }
-    } else { /* Respond for other request methods */
+    } 
+    else { // Respond for other request methods 
         return Response.redirect("https://github.com/cachecleanerjeet/amazon-scraper", 301)
     }
 }
