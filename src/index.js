e647a747e35ed7e7233e6e227d751b0b370b3777
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2023-09-18 18:52:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,30 +2,38 @@ addEventListener('fetch', (event) => {
     event.respondWith(handleRequest(event.request));
 });
 
-async function handleRequest(request) {
-    try{
-    const hardcodedUrl = 'https://www.amazon.in/dp/B0C4HBW653'; // Hardcoded Amazon product URL
+const cheerio = require('cheerio');
 
-    // Send an HTTP GET request to the hardcoded Amazon product page
-    const response = await fetch(hardcodedUrl);
+const hardcodedUrl = 'https://www.amazon.in/dp/'; // Hardcoded Amazon URL
+
+async function handleRequest(request) {
+    const params = new URLSearchParams(request.url.search); // Get query parameters
+    const productCodeParam = params.get('code'); // Extract 'code' parameter
+
+    if (!productCodeParam) {
+        return new Response('Missing product code in query parameter "code".', { status: 400 });
+    }
+
+    const productUrl = `${hardcodedUrl}${productCodeParam}`;
+
+    // Send an HTTP GET request to the combined Amazon product page URL
+    const response = await fetch(productUrl);
 
     if (response.ok) {
-        // Parse the HTML content
+        // Parse the HTML content using Cheerio
         const html = await response.text();
-        const dom = new DOMParser().parseFromString(html, 'text/html');
+        const $ = cheerio.load(html);
 
         // Find the image element and extract the source URL
-        const imageUrl = dom.querySelector('#landingImage').getAttribute('src');
+        const imageUrl = $('#landingImage').attr('src');
 
         if (imageUrl) {
-            return new Response(`Amazon Product Image URL: ${imageUrl}`, { status: 200 });
+            const responseText = `Amazon Product Image URL: ${imageUrl}\nProduct Code: ${productCodeParam}`;
+            return new Response(responseText, { status: 200 });
         } else {
             return new Response('Image not found on the page.', { status: 404 });
         }
     } else {
         return new Response('Error fetching the Amazon page.', { status: 500 });
-    }
-    }catch(err){
-        return new Response(`Error server. ${err?.message}`, { status: 500 });
     }
 }
